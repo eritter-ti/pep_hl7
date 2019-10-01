b99df5d150bee56f17d181942dc4e7aa8237d4f3
eritter-ti <eduardo.ritter@tecnologia.work>
2019-10-01 16:29:18 -0300

    Initial commit

---
+++ src/Dump20191001/pep_hl7_paciente.sql
@@ -0,0 +1,75 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: pep_hl7
+-- ------------------------------------------------------
+-- Server version	5.7.27-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `paciente`
+--
+
+DROP TABLE IF EXISTS `paciente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `paciente` (
+  `idPACIENTE` int(11) NOT NULL,
+  `NOME` varchar(45) DEFAULT NULL,
+  `SEXO` varchar(2) DEFAULT NULL,
+  `DATA_NASCIMENTO` date DEFAULT NULL,
+  `CPF` varchar(45) DEFAULT NULL,
+  `RG` varchar(45) DEFAULT NULL,
+  `TELEFONE` varchar(45) DEFAULT NULL,
+  `CELULAR` varchar(45) DEFAULT NULL,
+  `EMAIL` varchar(45) DEFAULT NULL,
+  `PAIS` varchar(45) DEFAULT NULL,
+  `UF` varchar(45) DEFAULT NULL,
+  `CIDADE` varchar(45) DEFAULT NULL,
+  `LOGRADOURO` varchar(45) DEFAULT NULL,
+  `BAIRRO` varchar(45) DEFAULT NULL,
+  `CEP` varchar(45) DEFAULT NULL,
+  `CARTAO_SUS` varchar(45) DEFAULT NULL,
+  `ESTADO_CIVIL` varchar(45) DEFAULT NULL,
+  `MAE` varchar(45) DEFAULT NULL,
+  `VACINA_APLICADA_idVACINA_APLICADA` int(11) NOT NULL,
+  `VACINA_APLICADA_DOSE_idDOSE` int(11) NOT NULL,
+  `VACINA_idVACINA` int(11) NOT NULL,
+  `VACINA_ESTOQUE_idESTOQUE` int(11) NOT NULL,
+  `historico_atendimento_idhistorico_atendimento` int(11) NOT NULL,
+  PRIMARY KEY (`idPACIENTE`,`VACINA_APLICADA_idVACINA_APLICADA`,`VACINA_APLICADA_DOSE_idDOSE`,`VACINA_idVACINA`,`VACINA_ESTOQUE_idESTOQUE`,`historico_atendimento_idhistorico_atendimento`),
+  KEY `fk_PACIENTE_VACINA_APLICADA1` (`VACINA_APLICADA_idVACINA_APLICADA`,`VACINA_APLICADA_DOSE_idDOSE`),
+  KEY `fk_PACIENTE_historico_atendimento1` (`historico_atendimento_idhistorico_atendimento`),
+  CONSTRAINT `fk_PACIENTE_VACINA_APLICADA1` FOREIGN KEY (`VACINA_APLICADA_idVACINA_APLICADA`, `VACINA_APLICADA_DOSE_idDOSE`) REFERENCES `vacina_aplicada` (`idVACINA_APLICADA`, `DOSE_idDOSE`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_PACIENTE_historico_atendimento1` FOREIGN KEY (`historico_atendimento_idhistorico_atendimento`) REFERENCES `historico_atendimento` (`idhistorico_atendimento`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `paciente`
+--
+
+LOCK TABLES `paciente` WRITE;
+/*!40000 ALTER TABLE `paciente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `paciente` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-10-01 16:26:07
